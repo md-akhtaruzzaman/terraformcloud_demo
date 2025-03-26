0ac6f39d8cc9cc5899928d1377f772d2e334cfa5
Md. Akhtaruzzaman <36110329+md-akhtaruzzaman@users.noreply.github.com>
2025-03-26 00:39:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ locals {
 }
 
 
-resource "aws_instance" "instance" {
+resource "aws_instance" "vpc" {
   ami = "ami-090fa75af13c156b4"
-  instance_type = "t2.2xlarge"
+  instance_type = "micro"
 
   tags = {
     project_name = var.project_name
